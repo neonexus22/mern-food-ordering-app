bd227cabe8d7a9fe54624ff1cf76e7699cea81d7
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-02-19 22:30:14 +0545

    added shadcn form and user-profile-page/form

---
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,120 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  email: z.string().email().optional(),
+  name: z.string().min(1, "Name is required."),
+  addressLine1: z.string().min(1, "Address Line 1 is required."),
+  country: z.string().min(1, "Country is required."),
+  city: z.string().min(1, "City is required."),
+});
+
+type UserFormData = z.infer<typeof formSchema>;
+
+type UserProfileFormProps = {
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+};
+const UserProfileForm = ({ onSave, isLoading }: UserProfileFormProps) => {
+  const form = useForm<UserFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSave)}
+          className="space-y-4 bg-gray-50 rounded-lg p-5 md:p-10"
+        >
+          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <FormDescription>
+            View and change your profile information here.
+          </FormDescription>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} disabled className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
+            <FormField
+              control={form.control}
+              name="addressLine1"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Address Line 1</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="country"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Country</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white w-full" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          {isLoading ? (
+            <LoadingButton />
+          ) : (
+            <Button type="submit" className="bg-orange-500">
+              Submit
+            </Button>
+          )}
+        </form>
+      </Form>
+    </>
+  );
+};
+export default UserProfileForm;
--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -5,12 +5,13 @@ import { ReactNode } from "react";
 
 type LayoutProps = {
   children: ReactNode;
+  showHero?: boolean;
 };
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, showHero = false }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero />
+      {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
