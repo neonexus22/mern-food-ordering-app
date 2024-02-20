fb3a2427b7b51d0814a65933ea6ce3dbdf60b0f3
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-02-20 18:41:58 +0545

    toast application

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,8 +1,20 @@
 import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
+import { toast } from "sonner";
 
 const UserProfilePage = () => {
-  const { mutate: updateUser, isLoading } = useUpdateMyUser();
+  const {
+    mutate: updateUser,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useUpdateMyUser();
+
+  if (isSuccess) {
+    toast.success("User updated successfully");
+  } else if (isError) {
+    toast.error("Error updating user");
+  }
 
   return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
 };
