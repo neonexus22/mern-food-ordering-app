54d999bcfb955801c6d87a3a8046454856fa61bf
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-02-25 17:59:37 +0545

    added search component and route FE

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,16 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
       <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
@@ -11,6 +20,10 @@ const HomePage = () => {
         <span className="text-xl text-gray-500">
           The best place to find the best food in the world.
         </span>
+        <SearchBar
+          placeHolder="Search by city or town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
